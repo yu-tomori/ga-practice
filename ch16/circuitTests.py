3253c8272ee3cd5244f2191f2880e95ff5ae63ff
yu <peacetomori1118@gmail.com>
2019-05-06 23:47:03 +0900

    now working on chapter16

--- ch16/circuitTests.py
+++ ch16/circuitTests.py
@@ -1,14 +1,14 @@
 import unittest
 import random
 import datetime
-
 import circuits
 import genetic
 
 def get_fitness(genes, rules, inputs):
 	circuit = nodes_to_circuit(genes)[0]
-	sourceLabels = "AB"
+	sourceLabels = "ABC"
 	rulesPassed = 0
+	# inputs = dict()
 	for rule in rules:
 		inputs.clear()
 		inputs.update(zip(sourceLabels, rule[0]))
@@ -32,24 +32,32 @@ def nodes_to_circuit(genes):
 		usedIndexes.append(used)
 	return circuit[-1], usedIndexes[-1]
 
-def create_gene(index, geneset):
-	gateType = random.choice(geneset)
+def create_gene(index, gates, sources):
+	if index < len(sources):
+		gateType = sources[index]
+	else:
+		gateType = random.choice(gates)
 	indexA = indexB = None
+	# Not and And
 	if gateType[1].input_count() > 0:
 		indexA = random.randint(0, index)
+	# And
 	if gateType[1].input_count() > 1:
-		indexB = random.randint(0, index) if index > 1 else 0
+		indexB = random.randint(0, index) if index > 1 and index >= len(sources) else 0
 		if indexB == indexA:
 			indexB = random.randint(0, index)
 	return Node(gateType[0], indexA, indexB)
 
-def mutate(childGenes, fnCreateGene):
+def mutate(childGenes, fnCreateGene, fnGetFitness, sourceCount):
 	count = random.randint(1, 5)
+	initialFitness = fnGetFitness(childGenes)
 	while count > 0:
 		count -=1
-		indexesUsed = [i for i in nodes_to_circuit(childGenes)[1]]
+		indexesUsed = [i for i in nodes_to_circuit(childGenes)[1] if i >= sourceCount]
 		index = random.choice(indexesUsed)
 		childGenes[index] = fnCreateGene(index)
+		if fnGetFitness(childGenes) > initialFitness:
+			return
 
 def display(candidate, startTime):
 	circuit = nodes_to_circuit(candidate.Genes)[0]
@@ -58,7 +66,10 @@ def display(candidate, startTime):
 
 class Node:
 	def __init__(self, createGate, indexA=None, indexB=None):
+		# createGateにはgeneset[.][0]が入る.
 		self.CreateGate = createGate
+		# Notの時は、indexAのみに値が入る.
+		# Sourceの時は、indexに値が入らない.
 		self.IndexA = indexA
 		self.IndexB = indexB
 
@@ -66,10 +77,9 @@ class CircuitTests(unittest.TestCase):
 	@classmethod
 	def setUpClass(cls):
 		cls.inputs = dict()
-		cls.geneset = [[circuits.And, circuits.And],
-						[lambda i1, i2: circuits.Not(i1), circuits.Not],
-						[lambda i1, i2: circuits.Source('A', cls.inputs), circuits.Source],
-						[lambda i1, i2: circuits.Source('B', cls.inputs), circuits.Source]]
+		cls.gates = [[circuits.And, circuits.And], [lambda i1, i2: circuits.Not(i1), circuits.Not]]
+		cls.sources = [[lambda i1, i2: circuits.Source('B', cls.inputs), circuits.Source],
+						[lambda i1, i2: circuits.Source('A', cls.inputs), circuits.Source]]
 
 	def test_generate_OR(self):
 		rules = [[[False, False], False],
@@ -79,6 +89,26 @@ class CircuitTests(unittest.TestCase):
 
 		optimalLength = 6
 		self.find_circuit(rules, optimalLength)
+
+	def test_generate_XOR(self):
+		rules = [[[False, False], False],
+				[[False, True], True],
+				[[True, False], True],
+				[[True, True], False]]
+		optimalLength = 11
+		self.find_circuit(rules, optimalLength)
+
+	def test_generate_AxBxC(self):
+		rules = [[[False, False, False], False],
+				[[False, False, True], True],
+				[[False, True, False], True],
+				[[False, True, True], False],
+				[[True, False, False], True],
+				[[True, False, True], False],
+				[[True, True, False], False],
+				[[True, True, True], True]]
+		self.sources.append([lambda l, r: circuits.Source('C', self.inputs), circuits.Source])
+		self.gates.append(circuits.Or, circuits.Or)
 
 	def find_circuit(self, rules, expectedLength):
 		maxLength = expectedLength
@@ -91,10 +121,10 @@ class CircuitTests(unittest.TestCase):
 			display(candidate, startTime)
 
 		def fnCreateGene(index):
-			return create_gene(index, self.geneset)
+			return create_gene(index, self.gates, self.sources)
 
 		def fnMutate(genes):
-			mutate(genes, fnCreateGene)
+			mutate(genes, fnCreateGene, fnGetFitness, len(self.sources))
 
 		def fnCreate():
 			return [fnCreateGene(i) for i in range(maxLength)]
--- ch16/circuits.py
+++ ch16/circuits.py
@@ -2,10 +2,6 @@ class Not:
 	def __init__(self, input):
 		self._input = input
 
-	@staticmethod
-	def input_count():
-		return 1
-
 	def get_output(self):
 		if self._input is None:
 			return None
@@ -13,36 +9,50 @@ class Not:
 		if value is None:
 			return None
 		return not value
-
+	
 	def __str__(self):
 		if self._input is None:
 			return "Not(?)"
 		return "Not({})".format(self._input)
 
-class And:
-	def __init__(self, inputA, inputB):
-		self._inputA = inputA
-		self._inputB = inputB
-
 	@staticmethod
 	def input_count():
-		return 2
+		return 1
+
+class GateWith2Inputs:
+	def __init__(self, inputA, inputB, label, fnTest):
+		self._inputA = inputA
+		self._inputB = inputB
+		self._label = label
+		self._fnTest = fnTest
 
 	def get_output(self):
 		if self._inputA is None or self._inputB is None:
 			return None
 		aValue = self._inputA.get_output()
-		bValue = self._inputB.get_output()
 		if aValue is None:
 			return None
+		bValue = self._inputB.get_output()
 		if bValue is None:
 			return None
-		return aValue and bValue
+		return self._fnTest(aValue, bValue)
 
 	def __str__(self):
 		if self._inputA is None or self._inputB is None:
-			return "And(?)"
-		return "And({} {})".format(self._inputA, self._inputB)
+			return "{}(?)".format(self._label)
+		return "{}({} {})".format(self._label, self._inputA, self._inputB)
+	
+	@staticmethod
+	def input_count():
+		return 2
+
+class And(GateWith2Inputs):
+	def init(self, inputA, inputB):
+		super().__init__(inputA, inputB, type(self).__name__, lambda a, b: a and b)
+
+class Or(GateWith2Inputs):
+	def __init__(self, inputA, inputB):
+		super().__init__(inputA, inputB, type(self).__name__, lambda a, b: a or b)
 
 class Source:
 	def __init__(self, sourceId, sourceContainer):
